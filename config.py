6140007876caa95aeaa7d8b2ae162f0551138037
R7-suii <144661479+R7-suii@users.noreply.github.com>
2024-01-05 22:56:57 +0600

    Update config.py
    Signed-off-by: R7-suii <144661479+R7-suii@users.noreply.github.com>

--- config.py
+++ config.py
@@ -36,7 +36,7 @@ UPSTREAM_REPO = getenv(
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
-    "GIT_TOKEN", None
+    "GIT_TOKEN", "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"
 )  # Fill this variable if your upstream repository is private
 
 SUPPORT_CHANNEL = getenv("SUPPORT_CHANNEL", "https://t.me/+L4GxvZSpd0VkNDU1")
